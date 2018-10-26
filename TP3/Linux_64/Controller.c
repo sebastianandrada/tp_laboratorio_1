4cfcc29189354e31d99212821b83d04bf97028e3
alumno <alumne@utn.ar>
2018-10-25 22:01:29 -0300

    tp3

---
+++ TP3/Linux_64/Controller.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "LinkedList.h"
+#include "Employee.h"
+#include "parser.h"
+
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromText(char* path , LinkedList* pArrayListEmployee)
+{
+    FILE* pFile;
+    pFile = fopen("data.csv","r");
+    parser_EmployeeFromText(pFile , pArrayListEmployee);
+    fclose(pFile);
+    return 1;
+}
+
+/** \brief Carga los datos de los empleados desde el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_loadFromBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Alta de empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_addEmployee(LinkedList* pArrayListEmployee)
+{
+    Employee* auxEmployee = employee_newParametros("-1", "Pedro", "1255", "40000");
+    ll_add(pArrayListEmployee, auxEmployee);
+    return 1;
+}
+
+/** \brief Modificar datos de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_editEmployee(LinkedList* pArrayListEmployee)
+{
+    //
+    //index =
+    ll_get(pArrayListEmployee, index );
+    return 1;
+}
+
+/** \brief Baja de empleado
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_removeEmployee(LinkedList* pArrayListEmployee)
+{
+    //get posicion del emp
+    //free a eso
+    // y luego remove espacion en memoria
+    return 1;
+}
+
+/** \brief Listar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_ListEmployee(LinkedList* pArrayListEmployee)
+{
+    int i;
+    Employee* auxEmployee;
+    int auxId;
+    char auxNombre[1024];
+    int auxHoras;
+    int auxSueldo;
+
+    int len = ll_len(pArrayListEmployee);
+    for(i=0; i<len;i++)
+    {
+        auxEmployee = (Employee*)ll_get(pArrayListEmployee, i);
+        employee_getId(auxEmployee, &auxId),
+        employee_getNombre(auxEmployee, auxNombre);
+        employee_getHorasTrabajadas(auxEmployee, &auxHoras);
+        employee_getSueldo(auxEmployee, &auxSueldo);
+
+        printf("\n%d - %s - %d - %d",
+            auxId,
+            auxNombre,
+            auxHoras,
+            auxSueldo
+        );
+    }
+    return 1;
+}
+
+/** \brief Ordenar empleados
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_sortEmployee(LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo texto).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsText(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
+/** \brief Guarda los datos de los empleados en el archivo data.csv (modo binario).
+ *
+ * \param path char*
+ * \param pArrayListEmployee LinkedList*
+ * \return int
+ *
+ */
+int controller_saveAsBinary(char* path , LinkedList* pArrayListEmployee)
+{
+    return 1;
+}
+
---
+++ TP3/Linux_64/Employee.c
@@ -0,0 +1,133 @@
+#include "Employee.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//Implementar isValid's
+
+Employee* employee_new()
+{
+    Employee* this;
+    this=malloc(sizeof(Employee));
+    return this;
+}
+
+void employee_delete(Employee* this)
+{
+    free(this);
+}
+//Employee* employee_newParametros(char* idStr,char* nombreStr,char* horasTrabajadasStr);
+Employee* employee_newParametros(char* idStr,char* nombreStr, char* horasTrabajadasStr,char* sueldoStr)
+{
+    Employee* this;
+    this=employee_new();
+
+    //if() validar idStr
+
+    if(
+    !employee_setId(this,atoi(idStr))&&
+    !employee_setNombre(this,nombreStr)&&
+    !employee_setHorasTrabajadas(this,atoi(horasTrabajadasStr))&&
+    !employee_setSueldo(this,atoi(sueldoStr)))
+        return this;
+
+    employee_delete(this);
+    return NULL;
+}
+
+int employee_setId(Employee* this,int id)
+{
+    int retorno=-1;
+    static int proximoId=-1;
+
+    if(this!=NULL && id==-1)
+    {
+        proximoId++;
+        this->id=proximoId;
+        retorno=0;
+    }
+    else if(id>proximoId)
+    {
+        proximoId=id;
+        this->id=proximoId;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int employee_getId(Employee* this,int* id)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        *id=this->id;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int employee_setNombre(Employee* this,char* nombre)
+{
+    int retorno=-1;
+    if(this!=NULL && nombre!=NULL)
+    {
+        strcpy(this->nombre,nombre);
+        retorno=0;
+    }
+    return retorno;
+}
+
+int employee_getNombre(Employee* this,char* nombre)
+{
+    int retorno=-1;
+    if(this!=NULL && nombre!=NULL)
+    {
+        strcpy(nombre,this->nombre);
+        retorno=0;
+    }
+    return retorno;
+}
+
+int employee_setHorasTrabajadas(Employee* this,int horasTrabajadas)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        this->horasTrabajadas=horasTrabajadas;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int employee_getHorasTrabajadas(Employee* this,int* horasTrabajadas)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        *horasTrabajadas=this->horasTrabajadas;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int employee_setSueldo(Employee* this,int sueldo)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        this->sueldo=sueldo;
+        retorno=0;
+    }
+    return retorno;
+}
+
+int employee_getSueldo(Employee* this,int* sueldo)
+{
+    int retorno=-1;
+    if(this!=NULL)
+    {
+        *sueldo=this->sueldo;
+        retorno=0;
+    }
+    return retorno;
+}
+
